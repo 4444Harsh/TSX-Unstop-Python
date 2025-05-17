e74fa6ec4b9f1600cbd08f1fb47cad1edf6a1357
Harsh <128832204+4444Harsh@users.noreply.github.com>
2025-05-17 23:08:06 +0530

    Update Topic_1_task.py

---
+++ Topic_1_task.py
@@ -0,0 +1,8 @@
+# Tasks:
+# 1. Install Python 3 on your computer.
+# 2. Install a code editor of your choice.
+# 3. Write a simple Python script that prints your name and a greeting.
+# 4. Experiment with running Python code directly in the interpreter.
+# 5. Write a script with comments explaining different parts of the
+# code. 
+print("Hello World My name is Harsh ")# in this we write to print the intro part which written inside the print function
--- intro.py
+++
@@ -1,1 +0,0 @@
-print("Hello World My name is Harsh ")# in this we write to print the intro part which written inside the print function
